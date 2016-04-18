76683a4882e3c5ea810c4cfd5fb2422e7de26e57
Karthika Pai <karthikapai@gmail.com>
2016-04-17 19:37:39 -0700

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,158 +1,414 @@
-# This is a simpy based  simulation of a M/M/1 queue system
 
+Gmail
+COMPOSE
+Labels
+Inbox
+Starred
+Sent Mail
+Career
+Junk
+LinkedIn
+Notes
+More 
+Chat
+Karthika Pai
+
+Search people...
+Available	en2fr
+Available	fr2en
+Available	Ian Chou
+Available	Lauren Tai
+Available	Patrick Lin
+Available	Robert Ying
+Idle (via Android mobile device)	Albert Deng
+Idle (via Android mobile device)	Albert Dinh
+Idle (via Android mobile device)	Alexander Zhu
+Idle (via Android mobile device)	Alric Siu
+Idle (via Android mobile device)	Alyssa Zhang
+Idle	Annie Yang
+Idle (via Android mobile device)	Ashwin Srikant
+Idle (via Android mobile device)	Austin Chen
+Idle (via Android mobile device)	Bhumika Kukreja
+Idle (via Android mobile device)	Brandon Liu
+Idle (via Android mobile device)	Brian Axelrod
+Idle (via Android mobile device)	Brian Deng
+Idle (via Android mobile device)	Calvin Ou
+Idle (via Android mobile device)	Candy
+Idle	Catherine Peng
+Idle (via Android mobile device)	Chengyi Lee
+Idle (via Android mobile device)	Deniz Oktay
+Idle (via Android mobile device)	Diane Kim
+Idle (via Android mobile device)	Divya Dhar
+Idle (via Android mobile device)	Divya Dhar
+Idle (via Android mobile device)	Ebin Kulakkattolikel
+Idle	Emily Yang
+Idle (via Android mobile device)	Franklin Lee
+Idle	Hansaa Gopalakrishnan
+Idle (via Android mobile device)	Harrison Wang
+Idle (via Android mobile device)	HLMIDNIGHT
+Idle (via Android mobile device)	Jango YuYu
+Idle (via Android mobile device)	Jason Pang
+Idle (via Android mobile device)	Jeffrey Chen
+Idle (via Android mobile device)	Jon K
+Idle (via Android mobile device)	Kaitlin Aquino
+Idle (via Android mobile device)	Karen O.
+Idle (via Android mobile device)	kartiga selvaganesan
+Idle (via Android mobile device)	kathynxiv
+Idle (via Android mobile device)	Kimberly
+Idle (via Android mobile device)	Lynbrook JETS
+Idle (via Android mobile device)	Navya Anil
+Idle (via Android mobile device)	pai.satheesh@gmail.com
+Idle (via Android mobile device)	Prima Shariff
+Idle (via Android mobile device)	Ramya
+Idle	Raymond Yu
+Idle (via Android mobile device)	Shalmali
+Idle (via Android mobile device)	Sharon
+Idle (via Android mobile device)	Shinray Kuo
+Idle (via Android mobile device)	Sreedevi Ashok
+Idle	Srikanth Kadiyala
+Idle (via Android mobile device)	Tanmay Bangalore
+Idle	Tejas Konduru
+Idle (via Android mobile device)	Thomas Okamoto
+Idle	Vicky Ro
+Idle (via Android mobile device)	Victoria Low
+Idle	Vignesh Geethan
+Idle (via Android mobile device)	Yu Jin
+Offline	Aditi
+Offline	Aishwarya 314
+Offline	Alex Lee
+Offline	Alice
+Offline	Alice Pang
+Offline	Alicyn Otoshi
+Offline	Allison Tani
+Offline	Alvin Chang
+Offline	Amanda Tam
+Offline	Ambika Bist
+Offline	Andy Yang
+Offline	Anika Dhamo
+Offline	Ann X
+Offline	Annia Barsova
+Offline	Annie Mei
+Offline	Apurva
+Offline	Arun Chakravorty
+Offline	Ashley
+Offline	Ashwini Kamath
+Offline	Avinash Ganesh
+Offline	betterdays123
+Offline	Brian Axelrod
+Offline	Carol Lin
+Offline	Cathy Wang
+Offline	Cecily
+Offline	Chinmay
+Offline	Chinmayi Manjunath
+Offline	David Liu
+Offline	Dennis Cui
+Offline	Diana
+Offline	Dinesh Jay
+Offline	Disha Banik
+Offline	Eric Shen
+Offline	Eric Xu
+Offline	Erica Yin
+Offline	Frances Guo
+Offline	frostbitten.store@gmail.com
+Offline	Gina Chacko
+Offline	gloria lin
+Offline	Hamza
+Offline	Hana
+Offline	Ilux P.
+Offline	Iris Chang
+Offline	Iris Shao
+Offline	Janet Chu
+Offline	Jennifer Chen
+Offline	Jessica Kim
+Offline	Jessica Kim
+Offline	Joseph Wei
+Offline	Journalism Club
+Offline	Julie Jin
+Offline	Justine Hsu
+Offline	karen lin
+Offline	Karun Pai
+Offline	Kathleen
+Offline	Kavya Shankar
+Offline	Keerthana Yellapragada
+Offline	Kevin Jiang
+Offline	knightgirl
+Offline	Kritika Iyer
+Offline	Landi Luo
+Offline	Lawrence Trask
+Offline	leslie tung
+Offline	Linda Xu
+Offline	lovingpink
+Offline	Lynbrook ArtReach
+Offline	Mandy Zen
+Offline	Manisha Vuppala
+Offline	Marcus Schorow
+Offline	Maya Parthasarathy
+Offline	Mayu Nishimura
+Offline	Megha
+Offline	Megha Pokhriyal
+Offline	Michael Hsiu
+Offline	Michael Luo
+Offline	Monisha Kashyap
+Offline	Mrudula Kothuri
+Offline	Nadya Anil
+Offline	Narayan Subramanian
+Offline	Natalie Qabazard
+Offline	Nina Kamath
+Offline	nivedita choudhuri
+Offline	Peggy Chiang
+Offline	Philip Hofman
+Offline	Pranav Mohan Shenoy
+Offline	Raisa R
+Offline	rashmi r
+Offline	Ravali Ambati
+Offline	Robert Chen
+Offline	Rohan
+Offline	Roopa Shankar
+Offline	Samantha Perez
+Offline	Shaheen Jeeawoody
+Offline	Sherry
+Offline	Siyuan Liu
+Offline	Soumya Kandukuri
+Offline	Sowmya Kotipalli
+Offline	Stephanie Kao
+Offline	Sunny Woan
+Offline	Tammy Luan
+Offline	Tom Ward
+Offline	Virup
+Offline	Vishal Kal
+Offline	Viswajit Simhadri
+Offline	Yifei Xie
+	Alexia  Invited
+	Alexia  Invited
+	apurva kulkarni
+	Brenda Hunt
+	Carole Wang  Invited
+	Cathy Wang  Invited
+	cinhuang@ucdavis.edu  Invited
+	elodie.chen  Invited
+	Emily Vu  Invited
+	Emily Yang  Invited
+	Emily, twin of Steph  Invited
+	jdle31  Invited
+	Jeremy Corbett  Invited
+	Kritika Sah  Invited
+	lasyasuma  Invited
+	megha Pokhriyal  Invited
+	michael_park123  Invited
+	Michel Chu  Invited
+	Miss Boots  Invited
+	Miss Boots  Invited
+	Miz diti  Invited
+	monishaa.saravanan@gmail.com  Invited
+	rmasad390  Invited
+	Ryan M.  Invited
+	Shaheen
+	Shaleena  Invited
+	Sunny Kung  Invited
+ 
+Move to Inbox More  Boomerang 
+1 of 21  
+ 
+Collapse all Print all In new window
+(no subject) 
+Inbox
+x 
+
+Kat N <kathynxiv@gmail.com>
+Attachments12/6/15
+
+to me 
+I added the hand simulation and I added more to your explanation. Could you read it over for any mistakes? Also I editted the code a bit and added better formatting for ease of reading and I took out a lot of unused variables. 
+
+Oh I wasn't sure how you added the code to the doc with that format, so please update the doc wtih the new code? 
+
+If everything ok ill handin the code. Could you also turn in the report tomorrow? It'd be weekdays tomorrow so I can't drive to campus and park without a permit. :(
+2 Attachments 
+ 
+
+Karthika Pai <karthikapai@gmail.com>
+12/6/15
+
+to Kat 
+Everything's fine. I'll print the report out and turn it in at 2:30 pm. :)
+
+
+Kat N <kathynxiv@gmail.com>
+12/6/15
+
+to me 
+YAY ok Do you want me to turn in code online? or did you do that already?
+
+
+Karthika Pai <karthikapai@gmail.com>
+12/6/15
+
+to Kat 
+Can you do that? Also pls put in a comment saying it's based on Python 2.
+
+Sent from my iPad
+
+	
+Click here to Reply or Forward
+4.55 GB (26%) of 17 GB used
+Manage
+Terms - Privacy
+Last account activity: 13 minutes ago
+Details
+Kat N
+Add to circles
+
+Show details
+
+
+#ECS 152 Project Part 2
+#Kathy Nguyen and Karthika Pai
+
+from __future__ import division
 import random
 import simpy
 import math
 
-RANDOM_SEED = 31
-SIM_TIME = 200000
-MU = 1
+TS = 1
+TOTALHOSTS = 10
+slot_length = 1
+LINEAR = 0
+EXPONENTIAL = 1
+SIM_TIME = 100000
+   
+class host:
+   def __init__(self, env, name, mylambda, ethernet):
+      self.server = simpy.Resource(env, capacity = 1)
+      self.env = env
+      self.name = name
+      self.mylambda = mylambda
+      self.L = 0              # Number of Packets in the queue/ Queue Length
+      self.S = 0              # Slot number for next transmission attempt
+      self.N = 0              # Number of reattempts to transmit packet
+      self.ethernet = ethernet
+      self.transmitpackets = 0#success_packets
+      self.flag_processing = 0
+      self.packet_number = 0
+      self.start_idle = 0
+
+#If a host has no packets in queue, it will not attempt to transmit
+#If a host has a packet in the queue:
+#  and the Ethernet's current slot number is the same as the host's slot number S, attempt to transmit
+#     succeed and process packet if there is only 1 host attempting to transmit
+#        decrease host's queue length, L, by 1 and increment it's slot number, S
+#     fails if there are multiple host's attempting to transmit
+#	 for each host attempting to transmit:
+#	    increment their number of reattempts, N, by 1
+#	    recalculate their slot numbers by given formula
+#	       exponential:  take K = min(N, 10), take a random decimal R =(0, 2^R)
+#		     S = S + R + 1
+#	       linear: take K = min(N, 1024), take a random int from 0, K
+#		     S = S + R + 1
+#
+   def process_packet(self, env):
+      start = env.now
+      if self.L == 0:
+        self.flag_processing = 0
+        self.start_idle_time = env.now
+      self.L -= 1
+      self.S += 1
+      self.N = 0 
+      self.transmitpackets += 1
+      
+   def packets_arrival(self, env, ethernet):
+      while True:
+         yield env.timeout(random.expovariate(self.mylambda))
+         arrival_time = env.now
+         if(self.L == 0):
+            self.S = ethernet.slot_number + 1
+         self.packet_number += 1
+         if self.flag_processing == 0:
+            self.flag_processing = 1
+         self.L += 1
+
+   def delay_packet(self, control):
+      #exponental
+      if control == 1: 
+         K = min(self.N, 10)
+         R = round(random.uniform(0, 2**K))
+         self.S = (self.S + R + 1)
+         self.N += 1
+      #linear
+      if control == 0: 
+         K = min(self.N, 1024)
+         R = random.randint(0, K)
+         self.S = self.S + R + 1
+         self.N += 1
 
 
+class ethernet:
+  def __init__(self, env, hostcount, mylambda, slot_length):
+      self.hostcount = hostcount
+      self.nodearray = []
+      self.env = env
+      self.mylambda = mylambda
+      self.slot_number = 0
+      self.slot_length = slot_length
+      self.slot_succeeded = 0
+      self.slot_collided = 0
+      self.hosts_transmitting = 0
+            
+  def ethernetdelay(self, env, control):
+      for x in range(self.hostcount):
+          self.nodearray.append(host(env, x + 1, self.mylambda, self))
+          self.env.process(self.nodearray[x].packets_arrival(self.env, self)) #create host array
 
-""" Queue system  """
-class server_queue:
-	def __init__(self, env, arrival_rate, Packet_Delay, Server_Idle_Periods, buffer_size):
-		self.server = simpy.Resource(env, capacity = 1)
-		self.env = env
-		self.queue_len = 0
-		self.flag_processing = 0
-		self.packet_number = 0
-		self.sum_time_length = 0
-		self.start_idle_time = 0
-		self.buffer_size = buffer_size#new
-		self.arrival_rate = arrival_rate
-		self.Packet_Delay = Packet_Delay
-		self.Server_Idle_Periods = Server_Idle_Periods
+      while True:
+         yield self.env.timeout(self.slot_length)
+         #reset count of number of hosts attempting to transmit
+         self.hosts_transmitting = 0
+         #For each host check if their current slot number matches, ethernet's currentr slot number
+         for x in range(self.hostcount):
+            if(self.nodearray[x].L == 0):
+               continue
+            if(self.nodearray[x].L >= 1 and self.nodearray[x].S == self.slot_number):
+               self.hosts_transmitting += 1
+               index = x
+         #If there is only one host transmitting, then it is successful
+         if (self.hosts_transmitting == 1):
+            self.slot_succeeded += 1
+            self.nodearray[index].process_packet(self.env)
+         #if there are multiple hosts attempting to transmit, delay the transmission and recalculate slot numbers
+         if (self.hosts_transmitting > 1):
+            self.slot_collided += 1
+            for x in range(self.hostcount):
+               if (self.nodearray[x].S == self.slot_number):
+                  self.nodearray[x].delay_packet(control) 
+	 #increment to process next slot
+         self.slot_number += 1						
 
-	def process_packet(self, env, packet):
-		with self.server.request() as req:
-			start = env.now
-			yield req
-			yield env.timeout(random.expovariate(MU))
-			latency = env.now - packet.arrival_time
-			self.Packet_Delay.addNumber(latency)
-			#print("Packet number {0} with arrival time {1} latency {2}".format(packet.identifier, packet.arrival_time, latency))
-			self.queue_len -= 1
-			if self.queue_len == 0:
-				self.flag_processing = 0
-				self.start_idle_time = env.now
-
-	def packets_arrival(self, env):
-		# packet arrivals
-
-		while True:
-		     # Infinite loop for generating packets
-			yield env.timeout(random.expovariate(self.arrival_rate))
-			  # arrival time of one packet
-
-			self.packet_number += 1
-			  # packet id
-			arrival_time = env.now
-			#print(self.num_pkt_total, "packet arrival")
-			new_packet = Packet(self.packet_number,arrival_time)
-			if self.queue_len >= self.buffer_size: #buffer_size exceeded--
-				continue
-			if self.flag_processing == 0:
-				self.flag_processing = 1
-				idle_period = env.now - self.start_idle_time
-				self.Server_Idle_Periods.addNumber(idle_period)
-				#print("Idle period of length {0} ended".format(idle_period))
-			self.queue_len += 1
-			env.process(self.process_packet(env, new_packet))
-
-
-""" Packet class """
-class Packet:
-	def __init__(self, identifier, arrival_time):
-		self.identifier = identifier
-		self.arrival_time = arrival_time
-
-
-class StatObject:
-    def __init__(self):
-        self.dataset =[]
-
-    def addNumber(self,x):
-        self.dataset.append(x)
-    def sum(self):
-        n = len(self.dataset)
-        sum = 0
-        for i in self.dataset:
-            sum = sum + i
-        return sum
-    def mean(self):
-        n = len(self.dataset)
-        sum = 0
-        for i in self.dataset:
-            sum = sum + i
-        return sum/n
-    def maximum(self):
-        return max(self.dataset)
-    def minimum(self):
-        return min(self.dataset)
-    def count(self):
-        return len(self.dataset)
-    def median(self):
-        self.dataset.sort()
-        n = len(self.dataset)
-        if n//2 != 0: # get the middle number
-            return self.dataset[n//2]
-        else: # find the average of the middle two numbers
-            return ((self.dataset[n//2] + self.dataset[n//2 + 1])/2)
-    def standarddeviation(self):
-        temp = self.mean()
-        sum = 0
-        for i in self.dataset:
-            sum = sum + (i - temp)**2
-        sum = sum/(len(self.dataset) - 1)
-        return math.sqrt(sum)
-
-
+  def throughput(self, control, linear, exponential):
+    if(control == 1):
+      exponential[self.mylambda] =  self.slot_succeeded/self.slot_number
+    elif(control == 0):
+      linear[self.mylambda] = self.slot_succeeded/self.slot_number
+      
+def print_table(mylambda, linear, exponential):
+   print("{:<8} {: <22} {: <18}".format(mylambda, exponential[mylambda], linear[mylambda]))
+   
 def main():
-	print("Make sure the WIDTH of the console is big enough to display table in one line.")
-	print("Simple queue system model:mu = {0} B = 10".format(MU))
-	print ("{0:<4} {1:<9} {2:<10} {3:<10} {4:<10} {5:<10} {6:<10} {7:<10} {8:<8} {9:<10}".format(
-        "Lambda", "Count", "Min", "Max", "Mean", "Median", "Sd", "Utilization", "Pd", "Calculated Pd"))
-	random.seed(RANDOM_SEED)
-	for arrival_rate in [0.2, 0.4, 0.6, 0.8, 0.9, 0.99]:
-		env = simpy.Environment()
-		Packet_Delay = StatObject()
-		Server_Idle_Periods = StatObject()
-		router = server_queue(env, arrival_rate, Packet_Delay, Server_Idle_Periods,10)
-		env.process(router.packets_arrival(env))
-		env.run(until=SIM_TIME)
-		print ("{0:<0.3f} {1:<10} {2:<10.3f} {3:<10.3f} {4:<10.3f} {5:<10.3f} {6:<10.3f} {7:<10.3f} {8:<10.3f} {9:<10.3f}".format(
-			round(arrival_rate, 3),
-			int(Packet_Delay.count()),
-			round(Packet_Delay.minimum(), 3),
-			round(Packet_Delay.maximum(), 3),
-			round(Packet_Delay.mean(), 3),
-			round(Packet_Delay.median(), 3),
-			round(Packet_Delay.standarddeviation(), 3),
-			round(1-Server_Idle_Periods.sum()/SIM_TIME, 3),
-			round((router.packet_number-Packet_Delay.count())/router.packet_number , 3),
-			round(((1-arrival_rate/MU)/(1-(arrival_rate/MU)**(router.buffer_size + 2)))*((arrival_rate/MU)**router.buffer_size), 3)))
-
-	print("Simple queue system model:mu = {0}, B = 50".format(MU))
-	print ("{0:<4} {1:<9} {2:<10} {3:<10} {4:<10} {5:<10} {6:<10} {7:<10} {8:<10} {9:<10}".format(
-        "Lambda", "Count", "Min", "Max", "Mean", "Median", "Sd", "Utilization", "Pd", "Calculated Pd"))
-	random.seed(RANDOM_SEED)
-	for arrival_rate in [0.2, 0.4, 0.6, 0.8, 0.9, 0.99]:
-		env = simpy.Environment()
-		Packet_Delay = StatObject()
-		Server_Idle_Periods = StatObject()
-		router = server_queue(env, arrival_rate, Packet_Delay, Server_Idle_Periods,50)
-		env.process(router.packets_arrival(env))
-		env.run(until=SIM_TIME)
-		print ("{0:<0.3f} {1:<10} {2:<10.3f} {3:<10.3f} {4:<10.3f} {5:<10.3f} {6:<10.3f} {7:<10.3f} {8:<10.3f} {9:<10.3f}".format(
-			round(arrival_rate, 3),
-			int(Packet_Delay.count()),
-			round(1-Server_Idle_Periods.sum()/SIM_TIME, 3),
-			round((router.packet_number-Packet_Delay.count())/router.packet_number , 3),
-			round(((1-arrival_rate/MU)/(1-(arrival_rate/MU)**(router.buffer_size + 2)))*((arrival_rate/MU)**router.buffer_size), 3)))
-
-
-
+   linear = {}
+   exponential = {}
+   print("Simulation Time: ", SIM_TIME)
+   print("{:<8} {:<22} {:<18}".format("Lambda", "Exponential Backoff", "Linear Backoff"))
+   for mylambda in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]:
+      #Simulate linear backoff
+      env = simpy.Environment()
+      myethernet = ethernet(env, TOTALHOSTS, mylambda, slot_length)
+      env.process(myethernet.ethernetdelay(env, LINEAR))
+      env.run(until=SIM_TIME)
+      myethernet.throughput(LINEAR, linear, exponential)
+      #Simulate exponential backoff
+      env = simpy.Environment()
+      myethernet = ethernet(env, TOTALHOSTS, mylambda, slot_length)
+      env.process(myethernet.ethernetdelay(env, EXPONENTIAL))
+      env.run(until=SIM_TIME)
+      myethernet.throughput(EXPONENTIAL, linear, exponential)
+      print_table(mylambda, linear, exponential)
+    
 if __name__ == '__main__': main()
-
